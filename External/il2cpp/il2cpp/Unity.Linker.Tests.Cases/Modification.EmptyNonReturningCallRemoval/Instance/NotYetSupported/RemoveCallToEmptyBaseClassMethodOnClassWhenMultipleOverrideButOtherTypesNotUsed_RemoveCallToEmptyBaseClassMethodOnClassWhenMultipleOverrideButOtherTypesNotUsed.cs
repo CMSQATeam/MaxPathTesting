abc010b3a465ce51b8eb6d1f6a0a6975f3acd5ae
CMSQATeam <43958307+CMSQATeam@users.noreply.github.com>
2019-03-07 18:39:25 +0530

    Update RemoveCallToEmptyBaseClassMethodOnClassWhenMultipleOverrideButOtherTypesNotUsed_RemoveCallToEmptyBaseClassMethodOnClassWhenMultipleOverrideButOtherTypesNotUsed.cs

--- External/il2cpp/il2cpp/Unity.Linker.Tests.Cases/Modification.EmptyNonReturningCallRemoval/Instance/NotYetSupported/RemoveCallToEmptyBaseClassMethodOnClassWhenMultipleOverrideButOtherTypesNotUsed_RemoveCallToEmptyBaseClassMethodOnClassWhenMultipleOverrideButOtherTypesNotUsed.cs
+++ External/il2cpp/il2cpp/Unity.Linker.Tests.Cases/Modification.EmptyNonReturningCallRemoval/Instance/NotYetSupported/RemoveCallToEmptyBaseClassMethodOnClassWhenMultipleOverrideButOtherTypesNotUsed_RemoveCallToEmptyBaseClassMethodOnClassWhenMultipleOverrideButOtherTypesNotUsed.cs
@@ -1,1 +1,24 @@
-Hello
+using System;
+
+namespace ConsoleApp2
+{
+    class RemoveCallToEmptyBaseClassMethodOnClassWhenMultipleOverrideButOtherTypesNotUsed_RemoveCallToEmptyBaseClassMethodOnClassWhenMultipleOverrideButOtherTypesNotUsed
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            p.PrintNumber(0);
+            Console.ReadKey();
+        }
+
+        void PrintNumber(int num)
+        {
+            Console.WriteLine(num);
+            if (num < 100)
+            {
+                PrintNumber(num + 1);
+            }
+        }
+    }
+}
