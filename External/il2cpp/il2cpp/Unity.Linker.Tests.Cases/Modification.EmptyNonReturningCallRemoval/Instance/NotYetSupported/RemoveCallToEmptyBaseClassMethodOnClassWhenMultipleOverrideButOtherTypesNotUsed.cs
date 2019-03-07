84c27bee8a3835860c8b7f9686b4fb22c3129880
Sandeep Badikolu <sbadikolu@rythmos.com>
2019-03-07 15:39:01 +0530

    added cs file with long text

---
+++ External/il2cpp/il2cpp/Unity.Linker.Tests.Cases/Modification.EmptyNonReturningCallRemoval/Instance/NotYetSupported/RemoveCallToEmptyBaseClassMethodOnClassWhenMultipleOverrideButOtherTypesNotUsed.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            p.PrintNumber(0);
+            Console.ReadKey();
+        }
+
+        void PrintNumber(int num)
+        {
+            Console.WriteLine(num);
+            if (num < 100)
+            {
+                PrintNumber(num + 1);
+            }
+        }
+    }
+}
